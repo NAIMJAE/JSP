d5688dfb1f46182d99be8f52fcba28ae9ec7d1fe
skdlawo5985 <skdlawo5985@gmail.com>
2024-02-01 12:16:45 +0900

    24.02.01(목)수업

---
+++ ch05/src/main/java/sub1/UserDTO.java
@@ -0,0 +1,46 @@
+package sub1;
+
+public class UserDTO {
+	
+	private String uid;
+	private String pass;
+	private String name;
+	private int age;
+	
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	// 기본생성자가 없으면 useBean 태그 사용 안됨
+	public UserDTO() {
+		
+	}
+
+	public String getUid() {
+		return uid;
+	}
+	public void setUid(String uid) {
+		this.uid = uid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "UserDTO [uid=" + uid + ", pass=" + pass + ", name=" + name + ", age=" + age + "]";
+	}
+}
